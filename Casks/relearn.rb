638165dc3ae743b7398b4ec5e1a09e044b7594f9
Ke Xin <ymx94yyds@gmail.com>
2026-07-25 03:25:05 +0000

    bump: relearn 2.1.30

--- Casks/relearn.rb
+++ Casks/relearn.rb
@@ -1,6 +1,6 @@
 cask "relearn" do
-  version "2.1.24"
-  sha256 "3742b21f31afa3d4dea38d08e4bfcf3d510655cee22c56b98d5a0f7d237da416"
+  version "2.1.30"
+  sha256 "31db5a45d3b30e1aee580db063bfccab5ef9ed19e2f6a3c19a348e10bbffd189"
 
   url "https://github.com/kexin94yyds/relearn-releases/releases/download/v#{version}/relearn-#{version}-arm64.dmg"
   name "relearn"
